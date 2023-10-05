c0c540c6c6696cb84610935372195def901419b2
RENZO REYMUNDO <rreymundor@hotmail.com>
2023-10-05 12:00:18 -0500

    Primer commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TF_Micro_2023_2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TF_Micro_2023_2.X.production.hex
